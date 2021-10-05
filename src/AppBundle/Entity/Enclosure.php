516aeaf41a54d3cc40a4cc9bf4b0bfb07305f6c8
eduardo <eduardo.bravo@geopagos.com>
2021-10-05 15:48:22 -0300

    Testing Exceptions

--- src/AppBundle/Entity/Enclosure.php
+++ src/AppBundle/Entity/Enclosure.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Entity;
 
+use AppBundle\Exception\NotABuffetException;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -31,11 +32,20 @@ class Enclosure
         return $this->dinosaurs;
     }
 
-    public function addDinosaur(Dinosaur $dinosaurs)
+    public function addDinosaur(Dinosaur $dinosaur)
     {
-        $this->dinosaurs[] = $dinosaurs;
+        if (!$this->canAddDinosaur($dinosaur)) {
+            throw new NotABuffetException();
+        }
+
+        $this->dinosaurs[] = $dinosaur;
     }
 
+    private function canAddDinosaur(Dinosaur $dinosaur): bool
+    {
+        return count($this->dinosaurs) === 0
+            || $this->dinosaurs->first()->isCarnivorous() === $dinosaur->isCarnivorous();
+    }
 
 
 }
--- tests/AppBundle/Entity/EnclosureTest.php
+++ tests/AppBundle/Entity/EnclosureTest.php
@@ -4,6 +4,7 @@ namespace Tests\AppBundle\Entity;
 
 use AppBundle\Entity\Dinosaur;
 use AppBundle\Entity\Enclosure;
+use AppBundle\Exception\NotABuffetException;
 use PHPUnit\Framework\TestCase;
 
 class EnclosureTest extends TestCase
@@ -23,5 +24,26 @@ class EnclosureTest extends TestCase
         $enclosure->addDinosaur(new Dinosaur());
 
         $this->assertCount(2, $enclosure->getDinosaurs());
+    }
+
+    public function testItDoesNotAllowCarnivorousDinosToMixWithHerbivores()
+    {
+        $enclosure = new Enclosure();
+        $enclosure->addDinosaur(new Dinosaur());
+
+        $this->expectException(NotABuffetException::class);
+
+        $enclosure->addDinosaur(new Dinosaur('Velociraptor', true));
+    }
+
+
+    public function testItDoesNotAllowToAddNonCarnivorousDinosaursToCarnivorousEnclosure()
+    {
+        $enclosure = new Enclosure();
+        $enclosure->addDinosaur(new Dinosaur('Velociraptor', true));
+        $this->expectException(NotABuffetException::class);
+        $enclosure->addDinosaur(new Dinosaur());
+
+
     }
 }
